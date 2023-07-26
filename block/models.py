c139e42d7cff570fc6d4c3b81d5741c4828aaa81
unnati <unnati@gmail.com>
2023-07-26 11:06:08 +0530

    blog file

---
+++ block/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
